ff47f4c31caa721785ab00c557ef20938c0dae2c
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-02 11:52:21 -0500

    Clase 8 - Parametros como referencia o como valor

--- clase1.js
+++ clase1.js
@@ -14,26 +14,14 @@ imprimirNombreEnMayuscula(aaron);
 imprimirNombreEnMayuscula(dario);
 imprimirNombreEnMayuscula({ nombre: 'Pepito' });
 
-imprimirNombreYEdad(aaron);
-imprimirNombreYEdad(dario);
-imprimirNombreYEdad({ nombre: 'Pepito', edad: 20 });
-
-imprimirNombreYEdad2(aaron);
-imprimirNombreYEdad2(dario);
-imprimirNombreYEdad2({ nombre: 'Pepito', edad: 20 });
-
 function imprimirNombreEnMayuscula(persona) {
   var { nombre } = persona;
   console.log(nombre.toUpperCase());
 }
 
-function imprimirNombreYEdad(persona) {
-  //Hola, me llamo NOMBRE y tengo EDAD años
-  var {nombre, edad} = persona;
-  console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`);
-}
-
-function imprimirNombreYEdad2({nombre, edad}) {
-  //Hola, me llamo NOMBRE y tengo EDAD años
-  console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`);
+function cumpleanos(persona) {
+  return {
+    ...persona,
+    edad: persona.edad + 1,
+  }
 }
