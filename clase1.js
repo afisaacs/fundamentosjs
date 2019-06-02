842ad9aeae4c0db69c52871e6e74ff3e1d7c576a
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-02 16:11:59 -0500

    Ciclo for

--- clase1.js
+++ clase1.js
@@ -1,60 +1,23 @@
 var aaron = {
   nombre: 'Aaron',
   apellido: 'Isaacs',
-  edad: 24,
-  ingeniero: true,
-  cocinero: false,
-  cantante: false,
-  guitarrista: true,
-  drone: true,
-};
-
-var kathe = {
-  nombre: 'Katherine',
-  apellido: 'Benites',
-  edad: 17,
-  ingeniero: true,
-  cocinero: false,
-  cantante: false,
-  guitarrista: true,
-  drone: true,
-};
-
-imprimirProfesiones(aaron);
-
-function imprimirProfesiones(persona) {
-  console.log(`${persona.nombre} es:`);
-  if(persona.ingeniero) {
-    console.log('Ingeniero');
-  }
-  if(persona.cocinero) {
-    console.log('Cocinero');
-  }
-  if(persona.cantante) {
-    console.log('Cantante');
-  }
-  if(persona.guitarrista) {
-    console.log('Guitarrista');
-  }
-  if(persona.drone) {
-    console.log('Piloto de drone');
-  }
-};
-const MAYORIA_DE_EDAD = 18;
-const esMayorDeEdad = ({ edad }) => edad >= MAYORIA_DE_EDAD;
-const esMenorDeEdad = (persona) => !esMayorDeEdad(persona);
-
-function imprimirSiEsMayorDeEdad(persona) {
-  var {nombre, edad} = persona;
-  if(esMayorDeEdad(persona)) {
-    console.log(`${nombre} es mayor de edad, (edad: ${edad})`);
-  } else {
-    console.log(`${nombre} es menor de edad, (edad: ${edad})`);
-  }
+  peso: 65,
 }
 
-function permitirAcceso(persona) {
-  if(esMenorDeEdad(persona)) {
-    console.log('Acceso denegado');
-  }
-};
+console.log(`Al inicio del año ${aaron.nombre} pesaba ${aaron.peso}Kg`)
+
+const INCREMENTO_DE_PESO = 0.2
+const DIAS_DEL_AGNO = 365
+const aumentarDePeso = persona => persona.peso += INCREMENTO_DE_PESO
+const disminuirPeso = persona => persona.peso -= INCREMENTO_DE_PESO
+
+for (var i = 0; i < DIAS_DEL_AGNO; i++) {
+  var random = Math.random()
+  if(random < 0.25){
+    aumentarDePeso(aaron)
+  } else if(random < 0.5) {
+    disminuirPeso(aaron)
+  } 
+}
+
+console.log(`Al final del año ${aaron.nombre} pesaba ${aaron.peso.toFixed(1)}Kg`)
