0e783e0d3740f42b5fd06ccaba4abb797ca5ab8b
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-02 17:18:39 -0500

    Ejemplo de signos

--- clase1.js
+++ clase1.js
@@ -1,9 +1,21 @@
-var contador = 0
+const horoscopos = {
+  aries: '21 marzo - 19 abril',
+  tauro: '20 abril - 20 mayo',
+  geminis: '21 mayo - 20 junio',
+  cancer: '21 junio - 22 julio',
+  leo: '23 julio - 22 agosto',
+  virgo: '23 agosto - 22 septiembre',
+  libra:'23 septiembre - 22 octubre',
+  escorpion: '23 octubre - 21 noviembre',
+  sagitario: '22 noviembre - 21 diciembre',
+  capricornio: '22 diciember - 19 enero',
+  acuario: '20 enero - 18 febrero',
+  piscis: '19 febrero - 20 marzo',
+};
 
-const llueve = () => Math.random() < 0.25
-
+const imprimirSigno = (signo) => console.log(horoscopos[signo]?horoscopos[signo]:'El signo no existe');
+let signo
 do {
-  contador++;
-} while (!llueve())
-
-console.log(`Fui a ver si llovía ${contador} ${contador===1?'vez':'veces'}`);
+  signo = prompt('¿Cuál es tu signo?')
+  imprimirSigno(signo);
+} while(signo && signo !== 'salir')
