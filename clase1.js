50034a31286a1b9dbd4d503521fd10708a45e2c5
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-02 16:55:44 -0500

    do while

--- clase1.js
+++ clase1.js
@@ -1,27 +1,9 @@
-var aaron = {
-  nombre: 'Aaron',
-  apellido: 'Isaacs',
-  peso: 65,
-}
+var contador = 0
 
-console.log(`Al inicio del año ${aaron.nombre} pesaba ${aaron.peso}Kg`)
+const llueve = () => Math.random() < 0.25
 
-const FACTOR_DE__CAMBIO_PESO = 0.3, DIAS_DEL_AGNO = 365, META = aaron.peso - 3;
-var dias = 0;
+do {
+  contador++;
+} while (!llueve())
 
-const aumentarDePeso = persona => persona.peso += FACTOR_DE__CAMBIO_PESO
-const disminuirDePeso = persona => persona.peso -= FACTOR_DE__CAMBIO_PESO
-const comeMucho = () => Math.random() < 0.3
-const realizaDeporte = () => Math.random() < 0.4
-
-while (aaron.peso > META) {
-  if(comeMucho()){
-    aumentarDePeso(aaron)
-  }
-  if(realizaDeporte()) {
-    disminuirDePeso(aaron)
-  }
-  dias++;
-}
-
-console.log(`Pasarion ${dias} hasta que ${aaron.nombre} adelgazó 3Kg, nuevo peso: ${aaron.peso.toFixed(1)}`)
+console.log(`Fui a ver si llovía ${contador} ${contador===1?'vez':'veces'}`);
