2fd643a81521c7fcbb789be290a7183ada990367
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-02 20:58:02 -0500

    Arrays

--- clase1.js
+++ clase1.js
@@ -1,21 +1,42 @@
-const horoscopos = {
-  aries: '21 marzo - 19 abril',
-  tauro: '20 abril - 20 mayo',
-  geminis: '21 mayo - 20 junio',
-  cancer: '21 junio - 22 julio',
-  leo: '23 julio - 22 agosto',
-  virgo: '23 agosto - 22 septiembre',
-  libra:'23 septiembre - 22 octubre',
-  escorpion: '23 octubre - 21 noviembre',
-  sagitario: '22 noviembre - 21 diciembre',
-  capricornio: '22 diciember - 19 enero',
-  acuario: '20 enero - 18 febrero',
-  piscis: '19 febrero - 20 marzo',
-};
+var sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  altura: 1.72
+}
 
-const imprimirSigno = (signo) => console.log(horoscopos[signo]?horoscopos[signo]:'El signo no existe');
-let signo
-do {
-  signo = prompt('¿Cuál es tu signo?')
-  imprimirSigno(signo);
-} while(signo && signo !== 'salir')
+var alan = {
+  nombre: 'Alan',
+  apellido: 'Perez',
+  altura: 1.86
+}
+
+var martin = {
+  nombre: 'Martin',
+  apellido: 'Gomez',
+  altura: 1.85
+}
+
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Juarez',
+  altura: 1.71
+}
+
+var vicky = {
+  nombre: 'Vicky',
+  apellido: 'Zapata',
+  altura: 1.56
+}
+
+var paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.76
+}
+
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+for (var i = 0; i < personas.length; i++) {
+  var persona = personas[i]
+  console.log(`${persona.nombre} mide ${persona.altura}`);
+}
