0a0c1eecce1eee0235d8cd9992644361c4b11daf
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-03 01:26:28 -0500

    La verdad oculta sobre las clases en Javascript

--- clase1.js
+++ clase1.js
@@ -1,4 +1,17 @@
+function HeredaDe(PrototipoHijo, PrototipoPadre) {
+  var fn = function () {}
+  fn.prototype = PrototipoPadre.prototype
+  PrototipoHijo.prototype = new fn
+  PrototipoHijo.prototype.constructor = PrototipoHijo
+}
+
 function Persona(nombre, apellido, altura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+function Desarrollador(nombre, apellido, altura) {
   this.nombre = nombre
   this.apellido = apellido
   this.altura = altura
@@ -8,17 +21,20 @@ Persona.prototype.saludar = function () {
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
 }
 Persona.prototype.soyAlto =  () => {
-  debugger
   return this.altura>1.8
+}
+HeredaDe(Desarrollador, Persona)
+Desarrollador.prototype.saludar = function() {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
 }
 
 var aaron = new Persona('Aaron', 'Isaacs', 1.65)
-var kathe = new Persona('Katherine', 'Benitez', 1.70)
-var sacha = new Persona('Sacha', 'Lifzyc', 1.95)
+var kathe = new Desarrollador('Katherine', 'Benitez', 1.70)
+// var sacha = new Persona('Sacha', 'Lifzyc', 1.95)
 
-aaron.saludar()
+// aaron.saludar()
 // aaron.soyAlto()
-kathe.saludar()
+// kathe.saludar()
 // kathe.soyAlto()
-sacha.saludar()
+// sacha.saludar()
 // sacha.soyAlto()
