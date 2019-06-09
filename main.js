f0632c77f909b7f730387ec14bcdb52f1a58f94f
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-09 11:34:31 -0500

    Manejo de errores con callback

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/'
-const opts = { crossDomain: true }
 
-// const onPeopleResponse = ({ name }) => console.log(`hola, yo soy ${name}`)
 const obtenerPersoaje = (id, callback) => {
   const url = `${API_URL}${PEOPLE_URL}${id}`
   fetch(url)
   .then(response => response.json())
-  .then(({ name }) => {
-    console.log(`hola, yo soy ${name}`)
-    if (callback) {
-      callback()
-    }
-  })
+  .catch(error => console.log(error))
+  .then(({ name }) => callback(name))
+  .catch(error => console.log(error))
 }
 
 const mostrarPersonajes = (inicio, fin) => {
-  obtenerPersoaje(inicio, () => {
+  obtenerPersoaje(inicio, name => {
+    console.log(`hola, yo soy ${name}`)
     if (inicio < fin) {
       mostrarPersonajes(inicio+1, fin)
     } else {
@@ -25,20 +21,4 @@ const mostrarPersonajes = (inicio, fin) => {
   })
 }
 
-mostrarPersonajes(1, 10)
-
-// obtenerPersoaje(1, 
-//   () => obtenerPersoaje(2, 
-//     () => obtenerPersoaje(3, 
-//       () => obtenerPersoaje(4,
-//         () => obtenerPersoaje(5,
-//           () => obtenerPersoaje(6,
-//             () => obtenerPersoaje(7,
-//               () => console.log('Fin')
-//             )
-//           )
-//         )
-//       )
-//     )
-//   )
-// )
+mostrarPersonajes(1, 30)
