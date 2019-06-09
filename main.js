08c440bfecffe5a3d61041b8cfd34a47dd496caf
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-09 10:15:16 -0500

    Manejando el orden y el asincronismo en JavaScript

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 const API_URL = 'https://swapi.co/api/'
-const PEOPLE_URL = 'people/:id'
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+const PEOPLE_URL = 'people/'
 const opts = { crossDomain: true }
 
-const onPeopleResponse = ({ name }) => console.log(`hola yo soy ${name}`)
+// const onPeopleResponse = ({ name }) => console.log(`hola, yo soy ${name}`)
+const obtenerPersoaje = (id, callback) => {
+  const url = `${API_URL}${PEOPLE_URL}${id}`
+  fetch(url)
+  .then(response => response.json())
+  .then(({ name }) => {
+    console.log(`hola, yo soy ${name}`)
+    if (callback) {
+      callback()
+    }
+  })
+}
 
-$.get(lukeUrl, opts, luke => onPeopleResponse(luke))
+const mostrarPersonajes = (inicio, fin) => {
+  obtenerPersoaje(inicio, () => {
+    if (inicio < fin) {
+      mostrarPersonajes(inicio+1, fin)
+    } else {
+      console.log('Fin')
+    }
+  })
+}
+
+mostrarPersonajes(1, 10)
+
+// obtenerPersoaje(1, 
+//   () => obtenerPersoaje(2, 
+//     () => obtenerPersoaje(3, 
+//       () => obtenerPersoaje(4,
+//         () => obtenerPersoaje(5,
+//           () => obtenerPersoaje(6,
+//             () => obtenerPersoaje(7,
+//               () => console.log('Fin')
+//             )
+//           )
+//         )
+//       )
+//     )
+//   )
+// )
