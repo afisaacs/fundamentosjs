24980edab8eef8f340e011a8fbd59ccd23feda5d
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-09 01:39:02 -0500

    El tiempo en javascript

--- clase1.js
+++ clase1.js
@@ -1,57 +1,9 @@
-class Persona {
-  constructor(nombre, apellido, altura) {
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
-  }
+let t1 = new Date()
+setTimeout(() => {
+  console.log('d')
+  let t2 = new Date()
+  console.log((t2-t1)/1000)
+}, 2000)
 
-  saludar(fn) {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
-    if (fn) {
-      fn(this.nombre, this.apellido)
-    }
-  }
-
-  soyAlto(){
-    return this.altura>1.8
-  }
+for (let i = 0; i < 1.e10; i++) {
 }
-
-class Desarrollador extends Persona{
-  constructor(nombre, apellido, altura) {
-    super(nombre, apellido, altura)
-  }
-
-  saludar(fn) {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
-    if (fn) {
-      fn(this.nombre, this.apellido, true)
-    }
-  }
-}
-
-function responderSaludo(nombre, apellido, esDev) {
-  console.log(`Buen día ${nombre} ${apellido}`);
-  if(esDev) {
-    console.log('Ah mirá, no sabía que eras dev');
-    
-  }
-  
-}
-
-var aaron = new Persona('Aaron', 'Isaacs', 1.65)
-var kathe = new Desarrollador('Katherine', 'Benitez', 1.70)
-var arturo = new Desarrollador('Arturo', 'Zapata', 1.80)
-
-aaron.saludar()
-kathe.saludar(responderSaludo)
-arturo.saludar(responderSaludo)
-
-// var sacha = new Persona('Sacha', 'Lifzyc', 1.95)
-
-// aaron.saludar()
-// aaron.soyAlto()
-// kathe.saludar()
-// kathe.soyAlto()
-// sacha.saludar()
-// sacha.soyAlto()
