9a5487419bc9eb3da47628cbee633a5650a3b002
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-09 22:33:55 -0500

    Promesas encadenadas

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/'
 
 const obtenerPersoaje = (id) => {
-
   return new Promise((resolve, reject) => {
     const url = `${API_URL}${PEOPLE_URL}${id}`
     $.get(url, {}, (data) => resolve(data))
@@ -10,19 +9,15 @@ const obtenerPersoaje = (id) => {
   })
 }
 
-obtenerPersoaje(1)
-.then(({ name }) => console.log(`El personaje 1 es ${name}`))
-.catch(id => console.log(`Sucedió un error al obtener el personaje  ${id}`))
+const mostrarPersonajes = (inicio, fin) => {
+  obtenerPersoaje(inicio)
+  .then(({ name }) => {
+    console.log(`El personaje ${inicio} es ${name}`)
+    if (inicio < fin) {
+      mostrarPersonajes(inicio+1, fin)
+    }
+  })
+  .catch(id => console.log(`Sucedió un error al obtener el personaje  ${id}`))
+}
 
-// const mostrarPersonajes = (inicio, fin) => {
-//   obtenerPersoaje(inicio, name => {
-//     console.log(`hola, yo soy ${name}`)
-//     if (inicio < fin) {
-//       mostrarPersonajes(inicio+1, fin)
-//     } else {
-//       console.log('Fin')
-//     }
-//   })
-// }
-
-// mostrarPersonajes(1, 30)
+mostrarPersonajes(1, 30)
