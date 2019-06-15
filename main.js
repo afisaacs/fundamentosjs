429f10875745a5230cd55094aeb66ddc285d29bb
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-15 00:25:24 -0500

    Empezando el juego

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-const API_URL = 'https://swapi.co/api/'
-const PEOPLE_URL = 'people/'
-
-const obtenerPersoaje = (id) => {
-  return new Promise((resolve, reject) => {
-    const url = `${API_URL}${PEOPLE_URL}${id}`
-    $.get(url, {}, (data) => resolve(data))
-    .fail(() => reject(id))
-  })
+const winLoad = () => {
+  btnStart = document.getElementById('btnstart')
+  btnStart.addEventListener('click', startGame)
 }
 
-const obtenerPersonajes = async () => {
-  let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,]
-  let promesas = ids.map(id => obtenerPersoaje(id))
-  try {
-    var personajes = await Promise.all(promesas)
-    console.log(personajes)
-  } catch (id) {
-    console.log(id)
-  }
-};
+const startGame = ({ target }) => {
+  let btnStart = target
+  let btnIndigo = document.querySelector('.indigo')
+  let btnPurple = document.querySelector('.purple')
+  let btnAmber = document.querySelector('.amber')
+  let btnLime = document.querySelector('.lime')
 
-obtenerPersonajes()
+  console.log(btnStart)
+  
+  class Game {
+    constructor () {
+      this.init()
+    }
+
+    init() {
+      btnStart.classList.add('hide')
+    }
+  }
+
+  var game = new Game()
+
+}
+
+window.addEventListener('load', winLoad)
