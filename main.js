957185760312e2194590657dc021a1987dda7a59
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-15 02:17:22 -0500

    Iluminando secuencia

--- main.js
+++ main.js
@@ -14,15 +14,47 @@ const startGame = ({ target }) => {
     constructor () {
       this.init()
       this.generateSecuence()
+      this.nextLevel()
     }
 
     init() {
       btnStart.classList.add('hide')
-      this.level = 1
+      this.level = 7
       this.colors = {indigo, purple, amber, lime}
     }
     generateSecuence() {
       this.secuence = new Array(10).fill(0).map(() => Math.floor(Math.random()*4))
+    }
+    nextLevel() {
+      this.iluminateSecuency()
+    }
+    async iluminateSecuency() {
+      for (let index = 0; index < this.level; index++) {
+        let color = this.numberToColor(this.secuence[index])
+        await this.iluminateColor(color)
+        await this.delay(300)
+      }
+    }
+    numberToColor(number) {
+      const mapNumberColor = {
+        0: this.colors.indigo,
+        1: this.colors.purple,
+        2: this.colors.amber,
+        3: this.colors.lime
+      }
+      return mapNumberColor[number]
+    }
+    iluminateColor(color) {
+      return new Promise(resolve => {
+        color.classList.add('light')
+        setTimeout(() => {
+          color.classList.remove('light')
+          resolve()
+        }, 350)
+      })
+    }
+    delay(number) {
+      return new Promise(resolve => setTimeout(() => resolve(), number))
     }
   }
 
